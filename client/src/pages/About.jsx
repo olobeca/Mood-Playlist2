cbbdb16535a32f96635167c7c5f461557e428d7a
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-04-28 12:13:33 +0200

    edited About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,6 +10,12 @@ function About() {
         <div>
             <h1>About</h1>
             <p>Welcome to the about page!</p>
+            <p>This application uses a webcam to capture images and analyze them using a Python backend.</p>
+            <p>It can detect emotions and suggest playlists based on the detected emotion.</p>
+            <p>To use the application, navigate to the home page and capture an image using the webcam.</p>
+            <p>The application will then analyze the image and provide you with a list of playlists that match your mood.</p>
+            <p>Enjoy your music!</p>
+            <p>Created by Aleksander Radecki</p>
         </div>
         </>
     )
